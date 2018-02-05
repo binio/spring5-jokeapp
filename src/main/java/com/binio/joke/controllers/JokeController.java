5739b688b934908bb151d8ca27f2ed73fa29a747
tomaszb <tbiniecki@gmail.com>
2018-02-05 23:54:14 +0000

    add controller

---
+++ src/main/java/com/binio/joke/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.binio.joke.controllers;
+
+import com.binio.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService js){
+        jokeService = js;
+    }
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
