1876474e721117fd1150e49d3dfac1eafdb861b6
tomaszb <tbiniecki@gmail.com>
2018-02-05 23:54:46 +0000

    serviceImpl

---
+++ src/main/java/com/binio/joke/services/JokeServiceImpl.java
@@ -0,0 +1,18 @@
+package com.binio.joke.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+    ChuckNorrisQuotes quotes;
+
+    public JokeServiceImpl() {
+        this.quotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return quotes.getRandomQuote();
+    }
+}
